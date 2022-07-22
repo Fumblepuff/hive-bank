bf55b6c3b82ecd21a522558828e9d6d8a931fa75
Fumblepuff <jhkalu@gmail.com>
2022-07-22 15:27:59 -0400

    created card widget

---
+++ lib/utils/card.dart
@@ -0,0 +1,122 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class MoneyCard extends StatelessWidget {
+  final String cardType;
+  final int cardNumber;
+  final double amount;
+  const MoneyCard({
+    Key? key,
+    required this.cardType,
+    required this.cardNumber,
+    required this.amount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10,
+                spreadRadius: 5,
+              ),
+            ],
+          ),
+          child: Stack(
+            children: [
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.2),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 20),
+                      const Icon(
+                        Icons.hive,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                      const SizedBox(width: 20),
+
+                      //Card Type
+                      Text(
+                        cardType,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 20),
+                    Text(
+                      '\$ $amount',
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: 200,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              '**** **** $cardNumber',
+                              style: TextStyle(
+                                color: Colors.grey[500],
+                                fontSize: 20,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Icon(
+                              Icons.money,
+                              color: Colors.grey[500],
+                              size: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

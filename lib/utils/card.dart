174f7de0bea89bfd834ab4300a0b33211780b43a
Fumblepuff <jhkalu@gmail.com>
2022-07-22 16:16:47 -0400

    added border to cards

--- lib/utils/card.dart
+++ lib/utils/card.dart
@@ -25,6 +25,10 @@ class MoneyCard extends StatelessWidget {
           decoration: BoxDecoration(
             color: Colors.transparent,
             borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: Colors.white,
+              width: 2,
+            ),
             boxShadow: const [
               BoxShadow(
                 color: Colors.grey,

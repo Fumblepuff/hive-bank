58b9d0c2f7d4621f4ebc1828990b20c6484c085c
Fumblepuff <jhkalu@gmail.com>
2022-07-25 13:36:19 -0400

    created login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,191 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _loginBox = Hive.box('login');
+  bool fade = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(milliseconds: 800), () {
+      setState(() {
+        fade = false;
+      });
+    });
+  }
+
+  void validate() {
+    if (_emailController.text.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Error'),
+          content: const Text('Please enter an email'),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ),
+      );
+    } else if (_passwordController.text.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Error'),
+          content: const Text('Please enter a password'),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ),
+      );
+    } else {
+      if (_loginBox.get(_emailController.text) == _passwordController.text) {
+        Navigator.pushNamed(context, '/home');
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Error'),
+            content: const Text('Invalid email or password'),
+            actions: [
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          ),
+        );
+      }
+      Navigator.pushNamed(context, '/home');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: fade ? 0 : 1,
+          duration: const Duration(milliseconds: 800),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.hive,
+                size: 170,
+                color: Colors.amber[800],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'H I V E',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 40,
+                ),
+              ),
+              Text(
+                'B A N K',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 20,
+                    ),
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              //const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 20,
+                    ),
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      validate();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber[800],
+                      textStyle: Theme.of(context).textTheme.headline6,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green[900],
+                      textStyle: Theme.of(context).textTheme.headline6,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
